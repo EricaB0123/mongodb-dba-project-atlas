eb41e8a145937a54c5c60c6c012c1712cd822991
EricaB0123 <erica.li.bradley@gmail.com>
2026-09-01 21:47:34 +1200

    Create clusterAdminRole with administrative permissions
    Defines a cluster administrator role with various cluster-level permissions.

--- MongoDB_Atlas_Configuration/rbac/ROLES/clusterMonitorRole.js
+++ MongoDB_Atlas_Configuration/rbac/ROLES/clusterMonitorRole.js
@@ -1,1 +1,40 @@
+/**
+ * Cluster Administrator Role
+ * Purpose: Provide cluster-level administrative permissions
+ * Scope: Entire cluster (not limited to a single database)
+ */
+
+use admin;
+
+db.createRole({
+  role: "clusterAdminRole",
+  privileges: [
+    {
+      resource: { cluster: true },
+      actions: [
+        "addShard",
+        "removeShard",
+        "enableSharding",
+        "flushRouterConfig",
+        "getShardMap",
+        "getShardVersion",
+        "listShards",
+        "replSetConfigure",
+        "replSetGetConfig",
+        "replSetGetStatus",
+        "replSetInitiate",
+        "replSetResizeOplog",
+        "replSetStepDown",
+        "replSetSyncFrom",
+        "setFeatureCompatibilityVersion",
+        "setParameter",
+        "serverStatus",
+        "connPoolStats",
+        "top",
+        "listDatabases"
+      ]
+    }
+  ],
+  roles: []
+});
 
