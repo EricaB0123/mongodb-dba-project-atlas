0989e0a6aedbbd3925278f08fc16726753c00f2f
EricaB0123 <erica.li.bradley@gmail.com>
2026-09-01 22:04:19 +1200

    Update DatabaseUserExample.js

--- MongoDB_Atlas_Configuration/rbac/users/DatabaseUserExample.js
+++ MongoDB_Atlas_Configuration/rbac/users/DatabaseUserExample.js
@@ -1,1 +1,13 @@
+/* Already have a user Created. 
+   This script adds an existing user by altering the user, to the testReadInsertRole created in rbac/roles.
+*/
+
+use admin;
+
+db.grantRolesToUser(
+  "srv########",                      <----- left user name blank on purpose.
+  [
+    { role: "testReadInsertRole", db: "admin" }
+  ]
+);
 
